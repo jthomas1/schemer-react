ae1072814cbcb3f089544bc81126a87da911f699
James Thomas <plinkplonk@gmail.com>
2021-03-28 09:38:11 +0100

    update comment

--- src/components/ColourControls/ColourControls.tsx
+++ src/components/ColourControls/ColourControls.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './ColourControls.css'
 import { useColourBarContext } from "../ColourBar/ColourBar";
 
-interface ColourControlsProps {}
+interface ColourControlsProps {
+}
 
 export const ColourControls: React.FC<ColourControlsProps> = () => {
     const { colour, updateRGB } = useColourBarContext()
 
-// Prevent interacting with the sliders causing drag and drop for the whole container
+    // Suppress any DOM events
+    // Used to prevent interacting with the sliders causing drag and drop for the whole container
     function suppressEvent(e: any) {
         e.preventDefault();
         e.stopPropagation();
@@ -25,33 +27,33 @@ export const ColourControls: React.FC<ColourControlsProps> = () => {
         <div
             draggable="false"
             className="colour-controls">
-            <label htmlFor={ `${ colour.id }-red` }>Red</label>
+            <label htmlFor={ `${ colour.id }-red` }>Red: { colour.rgb.red }</label>
             <input
                 draggable
                 onDragStart={ suppressEvent }
-                onChange={update}
+                onChange={ update }
                 id={ `${ colour.id }-red` }
                 data-colour="red"
                 type="range"
                 min="0"
                 max="255"
                 defaultValue={ colour.rgb.red }/>
-            <label htmlFor={ `${ colour.id }-green` }>Green</label>
+            <label htmlFor={ `${ colour.id }-green` }>Green: { colour.rgb.green }</label>
             <input
                 draggable
                 onDragStart={ suppressEvent }
-                onChange={update}
+                onChange={ update }
                 id={ `${ colour.id }-green` }
                 type="range"
                 data-colour="green"
                 min="0"
                 max="255"
                 defaultValue={ colour.rgb.green }/>
-            <label htmlFor={ `${ colour.id }-blue` }>Blue</label>
+            <label htmlFor={ `${ colour.id }-blue` }>Blue: { colour.rgb.blue }</label>
             <input
                 draggable
                 onDragStart={ suppressEvent }
-                onChange={update}
+                onChange={ update }
                 id={ `${ colour.id }-blue` }
                 type="range"
                 data-colour="blue"
